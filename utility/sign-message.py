266ae1879a190b79af66ec1abe6ab4d1a16e5583
cooparo <github.ambitious226@slmails.com>
2025-01-25 14:26:54 +0100

    removed message hashing
    the sign_deterministic() functions already hash the message with the
    hashfunc provided hash function

--- utility/sign-message.py
+++ utility/sign-message.py
@@ -50,7 +50,7 @@ def sign_message(private_key_ec: str, message: str) -> str:
         private_key = SigningKey.from_string(private_key_byte, curve=SECP256k1, hashfunc=sha256)
 
         # Decode and hash the message
-        data = sha256(message.encode()).digest()
+        data = message.encode()
 
         # Sign the message with the private key
         signature = private_key.sign_deterministic(data, sha256)
@@ -66,7 +66,7 @@ if __name__ == "__main__":
         print("Usage: sign-message.py <message> <private-key>")
         print()
         print("The private-key has to be in the WIF format.")
-        print("Eg. message-sign.py \"Who is John Galt?\" L21LJEeJwK35wby1BeTjwWssrhrgQE2MZrpTm2zbMC677czAHHu3")
+        print("Eg.\nmessage-sign.py \"Who is John Galt?\" L21LJEeJwK35wby1BeTjwWssrhrgQE2MZrpTm2zbMC677czAHHu3")
         sys.exit(1)
 
     message = sys.argv[1] 
